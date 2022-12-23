8e8cad7783d2791bb1dfa577a2dedb062488b54b
Marek Hering <marek.hering99@gmail.com>
2022-12-23 15:18:25 +0100

    add rhyme poetry generation

--- models/LSTMModel.py
+++ models/LSTMModel.py
@@ -9,6 +9,7 @@ from keras.callbacks import ModelCheckpoint
 import numpy as np
 import keras.backend as k
 
+from attention import Attention
 
 class LSTMModel:
     MODEL_PATH = 'data/models/LSTMModel/weights.h5'
@@ -28,21 +29,13 @@ class LSTMModel:
         return self
 
     def create_with_attention(self, total_words: int, output_dim, max_sequence_len: int, rnn_units: int = 256):
-        _in = Input(shape=(None,))
-        x = Embedding(total_words, output_dim, input_length=max_sequence_len - 1)(_in)
+        model_input = Input(shape=(None, ))
+        x = Embedding(total_words, output_dim, input_length=max_sequence_len - 1)(model_input)
         x = LSTM(rnn_units, return_sequences=True)(x)
-        x = LSTM(rnn_units, return_sequences=True)(x)
-        x = Dropout(0.2)(x)
-        e = Dense(1, activation='tanh')(x)
-        e = Reshape([-1])(e)
-        alpha = Activation('softmax')(e)
-        alpha_repeated = Permute([2, 1])(RepeatVector(rnn_units)(alpha))
-        c = Multiply()([x, alpha_repeated])
-        c = Lambda(lambda xin: k.sum(xin, axis=1), output_shape=(rnn_units,))(c)
-        _out = Dense(total_words, activation='softmax', name='pitch')(c)
-        model = Model([_in], [_out])
-        optimizer = Adam(learning_rate=0.001)
-        model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics='accuracy')
+        x = Attention(units=total_words)(x)
+        model = Model(model_input, x)
+        optimizer = Adam(learning_rate=0.005)
+        model.compile(loss='categorical_crossentropy', optimizer=optimizer)
         self.model = model
         return self
 
@@ -63,3 +56,8 @@ class LSTMModel:
     def predict(self, X):
         predict_x = self.model.predict([X], verbose=0)
         return np.argmax(predict_x, axis=1)
+
+
+if __name__ == '__main__':
+    LSTMModel().create_with_attention(9000, 2048, 15).model.summary()
+    LSTMModel().create(9000, 2048, 15).model.summary()
